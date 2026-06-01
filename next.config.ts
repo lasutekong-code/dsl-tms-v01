5fdf1104aa527bfb48d1a1237de8e0588d7f8127
lasutekong-code <lasute.kong@gmail.com>
2026-06-01 14:26:02 +0900

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   experimental: {
     proxyClientMaxBodySize: "20mb",
   },
-  // Turbopack + Vercel middleware .nft.json 생성 버그 회피를 위해 비활성화
-  // Next.js 16 + Turbopack 조합에서 middleware.js.nft.json 누락 오류 발생
-  // 참고: https://github.com/vercel/next.js/issues/71778
 };
 
 export default nextConfig;
