eaa85cc11a7a1ffbd22f49cfb8ecaf95b1a66036
lasutekong-code <lasute.kong@gmail.com>
2026-06-01 14:41:32 +0900

    Add files via upload

---
+++ src/app/admin/admins/new/admins_new_page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = "force-dynamic";
+
+import { AdminCreateForm } from "@/components/forms/admin-create-form";
+
+export default function AdminCreateAccountPage() {
+  return <AdminCreateForm />;
+}
